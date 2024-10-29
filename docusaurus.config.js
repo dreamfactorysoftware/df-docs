6c1c95f8741b014bb842d81f096346b80e0c6f60
Jacob Yi <jacobyideveloper@gmail.com>
2024-10-30 06:22:01 +0800

    #6 - Update navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,38 +4,48 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-const {themes: prismThemes} = require('prism-react-renderer');
+const { themes: prismThemes } = require("prism-react-renderer");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DreamFactory Docs',
-  tagline: 'Instant API Generation',
-  url: 'https://dreamfactory-docs.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'dreamfactorysoftware', // Usually your GitHub org/user name.
-  projectName: 'df-docs', // Usually your repo name.
+  title: "DreamFactory Docs",
+  tagline: "Instant API Generation",
+  url: "https://dreamfactory-docs.netlify.app",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "dreamfactorysoftware", // Usually your GitHub org/user name.
+  projectName: "df-docs", // Usually your repo name.
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-          editUrl:
-            'https://github.com/dreamfactorysoftware/df-docs/tree/main/',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
+          editUrl: "https://github.com/dreamfactorysoftware/df-docs/tree/main/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          editUrl: "https://github.com/dreamfactorysofware/df-docs/blog/",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -45,74 +55,82 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'DreamFactory',
+        title: "DreamFactory",
         logo: {
-          alt: 'DreamFactory Logo',
-          src: 'img/logo.png',
+          alt: "DreamFactory Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'intro',
-            position: 'left',
-            label: 'Documentation',
+            type: "docSidebar",
+            sidebarId: "intro",
+            position: "left",
+            label: "Documentation",
           },
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/dreamfactorysoftware/dreamfactory',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/dreamfactorysoftware/dreamfactory",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Documentation',
+            title: "Documentation",
             items: [
               {
-                label: 'Documentation',
-                to: '/intro',
+                label: "Documentation",
+                to: "/intro",
+              },
+              {
+                label: 'Blog',
+                to: '/blog'
               }
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
-              {label: "Github", href: "https://github.com/dreamfactorysoftware/dreamfactory"},
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/dreamfactory',
+                label: "Github",
+                href: "https://github.com/dreamfactorysoftware/dreamfactory",
               },
               {
-                label: 'Facebook',
-                href: 'https://www.facebook.com/dreamfactorysoftware',
+                label: "Twitter",
+                href: "https://twitter.com/dreamfactory",
               },
               {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/dreamfactory-software-inc',
+                label: "Facebook",
+                href: "https://www.facebook.com/dreamfactorysoftware",
               },
               {
-                label: 'YouTube',
-                href: 'https://www.youtube.com/@DreamfactorySoftware',
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/company/dreamfactory-software-inc",
+              },
+              {
+                label: "YouTube",
+                href: "https://www.youtube.com/@DreamfactorySoftware",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Legacy Blog',
-                to: 'https://blog.dreamfactory.com',
+                label: "Legacy Blog",
+                to: "https://blog.dreamfactory.com",
               },
               {
-                label: 'Legacy Wiki',
-                to: 'https://wiki.dreamfactory.com',
+                label: "Legacy Wiki",
+                to: "https://wiki.dreamfactory.com",
               },
               {
-                label: 'Legacy Docs',
-                to: 'https://guide.dreamfactory.com/docs',
-              }
+                label: "Legacy Docs",
+                to: "https://guide.dreamfactory.com/docs",
+              },
             ],
           },
         ],
